bf2473d08e4955811a067e98da38f82e3716f8b8
keets <928420597@qq.com>
2015-09-25 12:14:06 +0800

    first

---
+++ src/com/njit/service/CurriculumService.java
@@ -0,0 +1,14 @@
+package com.njit.service;
+
+import java.util.List;
+
+import com.njit.base.DaoSupport;
+import com.njit.domain.Curriculum;
+
+public interface CurriculumService extends DaoSupport<Curriculum>{
+
+	Curriculum getByName(String name);
+
+
+
+}
---
+++ src/com/njit/service/TaskService.java
@@ -0,0 +1,25 @@
+package com.njit.service;
+
+import java.util.List;
+
+import com.njit.base.DaoSupport;
+import com.njit.domain.Department;
+import com.njit.domain.Report;
+import com.njit.domain.Task;
+
+public interface TaskService extends DaoSupport<Task>{
+
+	List<Task> findByUser();
+
+	List<Task> findByStudent();
+
+	List<Task> findUnhandle(List<Report> reportList, List<Task> allTaskList);
+
+	List<Task> findByCourse(Long courseId);
+
+//	List<Department> findClassByTask(Long taskId);
+
+
+
+
+}
